07764cf0dce26acb60244aed6fa66f000f1ee892
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-10-01 13:33:09 +0200

    perf: add ENV vars to hide mongoDB IDs

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const cors = require('cors');
-const express = require('express');
-const app = express();
+const express = require('express')();
 const router = require('./router');
 const mongoose = require('mongoose');
+require("dotenv").config(); //COMMENT ON PROD
 
-mongoose.connect( 'mongodb+srv://MonUser:Dotsuka91210@cluster0.e54xl.mongodb.net/cook?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true } )
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.e54xl.mongodb.net/cook?retryWrites=true&w=majority`, 
+{useNewUrlParser: true, useUnifiedTopology: true } )
 .then(() => console.log('co OK'))
 .catch(() => console.log('co ERROR'));
 
-app.use(cors());
-app.use(express.json()); 
-app.use('/', router);
-app.listen(process.env.PORT || 3000);
+express.use(cors());
+express.use(express.json()); 
+express.use('/', router);
+express.listen(process.env.PORT || 3000);
